b1f420f392a8cfe741a55964fa6a2efdfb2b0787
doodle0 <49020517+doodle0@users.noreply.github.com>
2021-09-24 20:26:00 +0900

    remodeling 1

--- page_script.js
+++ page_script.js
@@ -64,7 +64,7 @@ function problemPageScript(problemDataFile) {
 
         let mainContainer = $("#main-container");
 
-        // 상단 nav 만들기
+        // 챔터 내비게이션 만들기
         let chapterNav = $('<ul id="chapter-nav" class="nav nav-tabs justify-content-around">');
         mainContainer.before(chapterNav);
 
@@ -90,25 +90,25 @@ function problemPageScript(problemDataFile) {
             mainContainer.append(chapterCard);
         }
         
-        // 앵커가 헤더에 가려지지 않게 스크롤 위치 수정
+        // 앵커가 내비게이션 바에 가려지지 않게 스크롤 위치 수정
         $('#chapter-nav .nav-link').click(function() {
             var divId = $(this).attr('href');
-            $('html, body').animate({ scrollTop: $(divId).offset().top - $('#header-area').height() }, 100);
+            $('html, body').animate({ scrollTop: $(divId).offset().top - $('#navbar').outerHeight() }, 10);
         });
     });
 }
 
 $(document).ready(function() {
     $.getJSON(resourceURL("resources/menus.json"), function(menus) {
-        let sidebar = $("#sidebar-ul, #sidebar-lg-ul");
+        let navbar = $("#navbar-ul, #navbar-lg-ul");
         for (let menu of menus) {
             // 구분선일 때
-            if (menu.type == "hr") {
-                sidebar.append($('<li class="my-1 border-top">'));
+            if (menu.type == "divider" || menu.type == "hr") {
+                navbar.append($('<li class="my-1 mx-1 border-top border-start">'));
             }
             // 실제 메뉴일 때
             else {
-                sidebar.append(
+                navbar.append(
                     $("<li>").append(
                         $('<a>')
                             .addClass(
@@ -124,7 +124,7 @@ $(document).ready(function() {
                 // i가 현재 페이지를 가리키는 메뉴일 때
                 // TODO: 제대로 고치기
                 if (window.location.pathname.endsWith(menu.link)) {
-                    $("#heading, #heading-lg").text($("#heading").text() + " - " + menu.menu_name);
+                    // $("#heading, #heading-lg").text($("#heading").text() + " - " + menu.menu_name);
                     document.title += " - " + menu.menu_name;
                     if (menu.problem_data) {
                         problemPageScript(menu.problem_data);
@@ -136,7 +136,7 @@ $(document).ready(function() {
 
     // 맨 위로 가기 버튼
     $('#to-top-btn').click(function() {
-        $('html, body').animate({ scrollTop: 0 }, 100);
+        $('html, body').animate({ scrollTop: 0 }, 0);
     });
     $(window).scroll(function() {
         if ($(this).scrollTop() > 200) {
